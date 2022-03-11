de6582e779a781da35ecffb2707003261d4bb4b4
Usman Memon <usman.menon@excellarate.com>
2022-03-11 18:40:43 +0530

    Remove commets

--- config-overrides.js
+++ config-overrides.js
@@ -5,11 +5,8 @@ module.exports = {
     loaders.splice(loaders.length - 1, 0, {
       test: /\.s[ac]ss$/i,
       use: [
-        // Creates `style` nodes from JS strings
         "style-loader",
-        // Translates CSS into CommonJS
         "css-loader",
-        // Compiles Sass to CSS
         "sass-loader",
       ],
     })
