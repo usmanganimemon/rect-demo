ca3d31b43ba5278b3d45b52012489345de0c4bd4
Usman Memon <usman.menon@excellarate.com>
2022-03-11 16:04:17 +0530

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+/* config-overrides.js */
+module.exports = {
+  webpack: function(config, env) {
+    let loaders = config.module.rules[1].oneOf
+    loaders.splice(loaders.length - 1, 0, {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    })
+    loaders.splice(loaders.length - 1, 0, {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    })
+    // return false
+    return config
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Home from './components/Home'
+
+import './assets/scss/App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Container>
+        <Home />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
